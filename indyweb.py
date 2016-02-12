73615a7cffabbf2212a64ca1defe9b3229991364
Richard Schneider <richard@schneiderbox.net>
2016-02-11 22:26:33 -0600

    basic engine wrapping complete

--- indyweb.py
+++ indyweb.py
@@ -1,9 +1,21 @@
-from flask import Flask
+import flask
 
-app = Flask(__name__)
+from subprocess import Popen, PIPE
+import sys
+
+app = flask.Flask(__name__)
 
 @app.route('/think/<qbn>')
 def think(qbn):
-	return qbn
+	p = Popen(sys.argv[1], stdin=PIPE, stdout=PIPE)
+	p.stdin.write(qbn);
+	p.stdin.flush()
+	p.wait()
+	qmn =p.stdout.readline().strip()
+
+	r = flask.Response('{"qmn": "%s"}' % qmn)
+	r.headers['Content-Type'] = 'application/json'
+	r.headers['Access-Control-Allow-Origin'] = '*'
+	return r
 	
 app.run(debug=True)
